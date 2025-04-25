3ffafb657a893fe45582d7cc503553b401b6a648
UsmanKhan555 <usmankhan540001@gmail.com>
2025-04-25 10:04:19 +0400

    updates security

--- weather-backend/config/settings.py
+++ weather-backend/config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lmh=n#@2g8-lir(py9=4=d#%(ls2ij5i2d&xjoqt@#n$+syudd'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-dev-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition
